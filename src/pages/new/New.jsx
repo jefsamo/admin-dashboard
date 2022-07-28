9683d69653d46e0963867782fa973799fcdabe35
Olawale Shopeyin <shopeyinwale@gmail.com>
2022-07-28 10:18:27 +0100

    Add new user page

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+import "./new.scss";
 
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadIcon className="icon" />
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+              <div className="formInput">
+                <label htmlFor="">Username</label>
+                <input type="text" placeholder="john_doe" id="" />
+              </div>
+              <div className="formInput">
+                <label htmlFor="">Full Name</label>
+                <input type="text" placeholder="John Doe" id="" />
+              </div>
+              <div className="formInput">
+                <label htmlFor="">Email</label>
+                <input type="email" placeholder="johndoe@gmail.com" id="" />
+              </div>
+              <div className="formInput">
+                <label htmlFor="">Phone</label>
+                <input type="text" placeholder="johndoe@gmail.com" id="" />
+              </div>
+              <div className="formInput">
+                <label htmlFor="">Password</label>
+                <input type="password" id="" />
+              </div>
+              <div className="formInput">
+                <label htmlFor="">Address</label>
+                <input type="text" placeholder="Igbogbo Ikorodu" id="" />
+              </div>
+              <div className="formInput">
+                <label htmlFor="">Country</label>
+                <input type="text" placeholder="Nigeria" id="" />
+              </div>
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
